d8c19428fbfcfa5760320c316af973b17a661704
Nuel <ibk.com@gmail.com>
2019-10-26 16:06:59 +0100

    Completed Module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,4 +41,8 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent phoneDialIntent = new Intent(Intent.ACTION_DIAL);
+        phoneDialIntent.setData(Uri.parse("tel:0123456789"));
+    }
 }
